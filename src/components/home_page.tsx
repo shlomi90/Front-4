7b495aa12e3522f012fd004dda9b077ca98ffccf
shlomi90 <shlomikaravani@gmail.com>
2024-03-13 17:12:40 +0200

    Project 2024 front

---
+++ src/components/home_page.tsx
@@ -0,0 +1,39 @@
+// Homepage.tsx
+import { useState, useEffect } from 'react';
+import PostCard from './post_card.tsx';
+import PostDetail from './post_detail.tsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../Style/homepage.css';
+
+
+function Homepage({ accessToken, posts, fetchPosts }: { accessToken: string, posts: any[], fetchPosts: () => void }) {
+  const [selectedPost, setSelectedPost] = useState(null);
+
+    useEffect(() => {
+      fetchPosts();
+    }, [accessToken]);
+
+  const handlePostClick = (post:any) => {
+    setSelectedPost(post);
+  };
+
+  const handlePostDetailClose = () => {
+    setSelectedPost(null);
+  };
+
+  return (
+    <div className="container">
+      <div className='posts-container'>
+        {posts.map((post) => (
+          <PostCard key={post.id} post={post} onPostClick={handlePostClick} />
+        ))}
+      </div>
+      {selectedPost && (
+        <PostDetail post={selectedPost} onClose={handlePostDetailClose} fetchPosts={fetchPosts} />
+      )}
+      
+    </div>
+  );
+}
+
+export default Homepage;
---
+++ src/services/user.tsx
@@ -0,0 +1,39 @@
+import { CredentialResponse } from '@react-oauth/google';
+import axios from 'axios';
+
+export interface IUser {
+  email: string;
+  password?: string;
+  username?: string;
+  imgURL?: string;
+  id?: string;
+  accessToken?: string; // Now using 'accessToken' without colon
+  refreshToken?: string; // Now using 'refreshToken' without colon
+}
+
+ export const googleSignIn = async (credentialResponse: CredentialResponse) => {
+    return new Promise<any>((resolve, reject) => {
+    console.log('googleSignIn..');
+    axios.post('https://10.10.248.205/auth/google', {
+        credentialResponse: credentialResponse
+    }).then((response) => {
+        console.log(response);
+        localStorage.setItem('accessToken',response.data['access token:']);
+        localStorage.setItem('refreshToken',response.data['refresh token:'])
+        localStorage.setItem('userId', response.data['user id:']);
+        localStorage.setItem('userName', response.data['username:']);
+        localStorage.setItem('imgURL', response.data['imgURL:']);
+        localStorage.setItem('email', response.data['email:']);
+        localStorage.setItem('posts', JSON.stringify(response.data.posts));
+       
+
+      resolve(response.data);
+    }).catch((error) => {
+      console.error('Google sign in failed', error);
+      reject(error);}
+    );
+  }
+ )}
+ 
+
+ 
